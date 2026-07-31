7c29f0ea2b63c5356c21498c412db156f0794f2f
K2976 <sanghikartik2976@gmail.com>
2026-07-31 23:16:28 +0530

    initial commit

---
+++ playback-popup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  playback-popup
+//
+//  Created by Kartik on 31-07-2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ playback-popup/playback_popupApp.swift
@@ -0,0 +1,17 @@
+//
+//  playback_popupApp.swift
+//  playback-popup
+//
+//  Created by Kartik on 31-07-2026.
+//
+
+import SwiftUI
+
+@main
+struct playback_popupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
